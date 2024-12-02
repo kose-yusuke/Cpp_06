3c3d231cc936e27d75de77c537142829f8b116e2
yusuke koseki <yusuke188alc@icloud.com>
2024-12-02 16:30:17 +0900

    ex01 first commit

---
+++ ex01/Serializer.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Serializer.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: koseki.yusuke <koseki.yusuke@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/30 22:50:26 by koseki.yusu       #+#    #+#             */
+/*   Updated: 2024/12/02 16:29:33 by koseki.yusu      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <cmath>
+
+class Serializer{
+    
+};
